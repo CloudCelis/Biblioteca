fa92b893597aeb9bea18fab23bf1198e0d745e54
CloudCelis <marco.celis.r@gmail.com>
2018-07-02 02:49:06 -0400

    Version del domingo 26 del 6 se mejoro lo obtencion del usuario para la gestion del perfil se agrego clase singleton, aun no se implementa

---
+++ src/Servicios/Validaciones.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Servicios;
+
+import java.text.NumberFormat;
+import java.text.ParsePosition;
+
+/**
+ *
+ * @author marco
+ */
+public class Validaciones 
+{
+    public static boolean isNumeric(String str)
+    {
+        NumberFormat formatter = NumberFormat.getInstance();
+        ParsePosition pos = new ParsePosition(0);
+        formatter.parse(str, pos);
+        return str.length() == pos.getIndex();
+    }
+}
